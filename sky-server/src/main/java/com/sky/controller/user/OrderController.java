913c94b6f3a79bf8412433c5fe6c266b8083ff3c
zxd102914 <1474148406@qq.com>
2025-07-09 11:36:56 +0800

    催单提醒模块代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -112,7 +112,18 @@ public class OrderController {
     }
 
 
-
+    /**
+     * 用户催单
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/reminder/{id}")
+    @ApiOperation("用户催单")
+    public Result reminder(@PathVariable("id") Long id) {
+        orderService.reminder(id);
+        return Result.success();
+    }
 
 
 }
